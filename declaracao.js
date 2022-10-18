f296ce2029f092b9b904f14b46030975cb5c4835
lucaspaulamonti <lucasdepaulamonti@gmail.com>
2022-10-18 00:01:00 -0300

    Create declaracao.js

---
+++ declaracao.js
@@ -0,0 +1,10 @@
+// Declarando variáveis com as três possibilidades.
+let str1 = 'Lucas';
+let str2 = String();
+let str3;
+let num1 = 10
+let num2 = Number();
+let num3;
+let bol1 = true;
+let bol2 = Boolean();
+let bol3;
